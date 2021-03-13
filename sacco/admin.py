d1ff9ad6bfd5bf19da02f8c1396d4dea4abe51f5
kadweka <husseinkatana17@gmail.com>
2021-03-13 20:12:29 +0300

    add models to the admin console

--- sacco/admin.py
+++ sacco/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SaccoManager,SaccoMatatu,MatatuConductor,MatatuDriver,MatatuCollect
 
-# Register your models here.
+admin.site.register(SaccoManager)
+admin.site.register(SaccoMatatu)
+admin.site.register(MatatuConductor)
+admin.site.register(MatatuDriver)
+admin.site.register(MatatuCollect)
