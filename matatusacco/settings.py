d0ec1828ae9126f2937a32aefff1bec4114ec808
kadweka <husseinkatana17@gmail.com>
2021-03-13 19:36:42 +0300

    add a database configurations

--- matatusacco/settings.py
+++ matatusacco/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'matatusacco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saccomatatu',
+        'USER': 'kadweka',
+    'PASSWORD':'matatu',
     }
 }
 
